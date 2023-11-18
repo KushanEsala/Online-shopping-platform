fbeabe3371f2c1d20f709ab01d4607ec7006a395
KushanEsala <kushanesalakck@gmail.com>
2023-11-19 01:29:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -129,31 +129,6 @@ include("includes/main.php");
         </div>
       </div>
 
-      <!-- <div class="banners">
-        <div class="container clearfix">
-
-          <div class="banner-award">
-            <span>Award winner</span><br> Fashion awards 2016
-          </div>
-
-          <div class="banner-social">
-            <a href="#" class="banner-social__link">
-            <i class="icon-facebook"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-twitter"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-instagram"></i>
-          </a>
-            <a href="#" class="banner-social__link">
-            <i class="icon-pinterest-circled"></i>
-          </a>
-          </div>
-
-        </div>
-      </div> -->
-
       <div class="page-footer__subline">
         <div class="container clearfix">
 
